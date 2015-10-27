e9dfbed6e803ff04cdf38f3461d1924c8e9ec46b
Ronald Huereca <ronalfy@gmail.com>
2015-10-27 05:06:09 -0500

    Translating settings screen for admin.

--- templates/settings-intro.php
+++ templates/settings-intro.php
@@ -1,20 +1,20 @@
 <div id="social-welcome">
-<p>Postmatic Social Commenting takes a lighter weight approach than traditional Social Login plugins. </p>
+<p><?php _e( 'Postmatic Social Commenting takes a lighter weight approach than traditional Social Login plugins.', 'postmatic-social' ); ?></p>
 
-<h1>The big difference</h1>
+<h1><?php _e( 'The big difference', 'postmatic-social' ); ?></h1>
 
-<p>No WordPress users are created. Nobody is logged in. This isn't <em>social login</em>, it's <em>social commenting</em>. That means it's simple, lean, and <strong>extremely fast</strong>.</p>
+<p><?php _e( 'No WordPress users are created. Nobody is logged in. This is not <em>social login</em>, it is <em>social commenting</em>. That means it is simple, lean, and <strong>extremely fast</strong>.</p>', 'postmatic-social' ); ?>
 
-<h1>How it works</h1>
+<h1><?php _e( 'How it works', 'postmatic-social' ); ?></h1>
 
 <ol>
-<li>Users click the social network icon of their choice when they want to leave a comment.</li>
-<li>The social network asks their permission to share their name, profile image, and email address with your site.</li>
-<li>If they agree A standard WordPress cookie is added to their browser and the comment form is ready for their reply. When they return to your site they are already authorized.</li>
+<li><?php _e( 'Users click the social network icon of their choice when they want to leave a comment.', 'postmatic-social' ); ?></li>
+<li><?php _e( 'The social network asks their permission to share their name, profile image, and email address with your site.', 'postmatic-social' ); ?></li>
+<li><?php _e( 'If they agree A standard WordPress cookie is added to their browser and the comment form is ready for their reply. When they return to your site they are already authorized.', 'postmatic-social' ); ?></li>
 </ol>
 
-<h1>Get started</h1>
+<h1><?php _e( 'Get started', 'postmatic-social' ); ?></h1>
 
-<p>On the tabs above you can choose which networks you would like to add to your comment form. Configuration with each social network can be <em>oh so no fun</em>. That's why we've provided some videos to walk you through it. Just think about all the time you'll save your users and all the nice new comments you'll receive.</p>
+<p><?php _e( 'On the tabs above you can choose which networks you would like to add to your comment form. Configuration with each social network can be <em>oh so no fun</em>. That is why we have provided some videos to walk you through it. Just think about all the time you will save your users and all the nice new comments you will receive.', 'postmatic-social' ); ?></p>
 
 </div>
