7b61b256e3697bc17945769108bde02130f4eee0
Ronald Huereca <rhuereca@bigwing.com>
2015-10-29 07:57:12 -0500

    Coding standards and data output sanitization.

--- functions/Postmatic_Social_Comments_Tab.php
+++ functions/Postmatic_Social_Comments_Tab.php
@@ -6,7 +6,7 @@ abstract class Postmatic_Social_Comments_Tab
 
     public function __construct()
     {
-        $this->admin_ajax_url = admin_url('admin-ajax.php');
+        $this->admin_ajax_url = admin_url( 'admin-ajax.php' );
         $this->init();
     }
 
@@ -17,23 +17,23 @@ abstract class Postmatic_Social_Comments_Tab
     function get_settings()
     {
         $default_settings = $this->get_default_settings();
-        $tab_id = $default_settings['id'];
-        $all_settings = get_option('postmatic-social');
-        if (!isset($all_settings) || !is_array($all_settings)) {
+        $tab_id = $default_settings[ 'id' ];
+        $all_settings = get_option( 'postmatic-social' );
+        if ( ! isset( $all_settings ) || ! is_array( $all_settings ) ) {
             $all_settings = array();
         }
-        if (!array_key_exists($tab_id, $all_settings)) {
+        if ( ! array_key_exists( $tab_id, $all_settings ) ) {
             $sn_settings = array();
-            $all_settings[$tab_id] = $sn_settings;
+            $all_settings[ $tab_id ] = $sn_settings;
         } else {
-            $sn_settings = $all_settings[$tab_id];
+            $sn_settings = $all_settings[ $tab_id ];
         }
-        foreach ($default_settings["fields"] as $field_id => $field_meta) {
-            if (!array_key_exists($field_id, $sn_settings)) {
-                $sn_settings[$field_id] = $field_meta['default_value'];
+        foreach ( $default_settings[ "fields" ] as $field_id => $field_meta ) {
+            if ( ! array_key_exists( $field_id, $sn_settings ) ) {
+                $sn_settings[ $field_id ] = $field_meta[ 'default_value' ];
             }
         }
-        return apply_filters('pms_comments_settings', $sn_settings, $tab_id);
+        return apply_filters( 'pms_comments_settings', $sn_settings, $tab_id );
     }
 
     function save_settings()
@@ -41,19 +41,19 @@ abstract class Postmatic_Social_Comments_Tab
         $tab_id = $this->get_id();
         $default_settings = $this->get_default_settings();
         $settings = $this->get_settings();
-        foreach ($default_settings['fields'] as $field_id => $field_meta) {
-            if (array_key_exists($field_id, $_POST)) {
-                $settings[$field_id] = $_POST[$field_id];
+        foreach ( $default_settings[ 'fields' ] as $field_id => $field_meta ) {
+            if ( array_key_exists( $field_id, $_POST ) ) {
+                $settings[ $field_id ] = $_POST[ $field_id ];
             } else {
-                $settings[$field_id] = $field_meta['default_value'];
+                $settings[ $field_id ] = $field_meta[ 'default_value' ];
             }
         }
-        $all_settings = get_option('postmatic-social');
-        if (!isset($all_settings) || !is_array($all_settings)) {
+        $all_settings = get_option( 'postmatic-social' );
+        if ( ! isset( $all_settings ) || ! is_array( $all_settings ) ) {
             $all_settings = array();
         }
-        $all_settings[$tab_id] = $settings;
-        update_option('postmatic-social', $all_settings);
+        $all_settings[ $tab_id ] = $settings;
+        update_option( 'postmatic-social', $all_settings );
     }
 
     abstract function get_default_settings();
@@ -61,46 +61,46 @@ abstract class Postmatic_Social_Comments_Tab
     function get_id()
     {
         $default_settings = $this->get_default_settings();
-        return $default_settings['id'];
+        return $default_settings[ 'id' ];
     }
 
     function get_title()
     {
         $default_settings = $this->get_default_settings();
-        return $default_settings['title'];
+        return $default_settings[ 'title' ];
     }
 
     abstract function render_settings_admin_page();
 
-    protected function render_form_field($field_id, $field_value, $field_meta)
+    protected function render_form_field( $field_id, $field_value, $field_meta )
     {
-        $field_type = $field_meta['type'];
-        switch ($field_type) {
+        $field_type = $field_meta[ 'type' ];
+        switch ( $field_type ) {
             case 'text':
             {
                 echo '<tr>';
-                echo '<th><label for="' . $field_id . '_id">' . $field_meta['title'] . '</label></th>';
-                echo '<td><input type="text" id="' . $field_id . '_id" name="' . $field_id . '" value="' . $field_value . '" class="regular-text"></td>';
+                echo '<th><label for="' . esc_attr( $field_id ) . '_id">' . esc_html( $field_meta['title'] ) . '</label></th>';
+                echo '<td><input type="text" id="' . esc_attr( $field_id ) . '_id" name="' . esc_attr( $field_id ) . '" value="' . esc_attr( $field_value ) . '" class="regular-text"></td>';
                 echo '</tr>';
                 break;
             }
             case 'switch':
             {
                 echo '<tr>';
-                echo '<th><label for="' . $field_id . '_id">' . $field_meta['title'] . '</label></th>';
-                echo '<td><div class="pms-toggle toggle-modern toggle" data-input-id="' . $field_id . '_id"></div><input type="hidden" id="' . $field_id . '_id" name="' . $field_id . '" value="' . $field_value . '"></td>';
+                echo '<th><label for="' . esc_attr( $field_id ) . '_id">' . esc_html( $field_meta['title'] ) . '</label></th>';
+                echo '<td><div class="pms-toggle toggle-modern toggle" data-input-id="' . esc_attr( $field_id ) . '_id"></div><input type="hidden" id="' . esc_attr( $field_id ) . '_id" name="' . esc_attr( $field_id ) . '" value="' . esc_attr( $field_value ) . '"></td>';
                 echo '</tr>';
                 break;
             }
             case 'select':
             {
                 echo '<tr>';
-                echo '<th scope="row"><label for="' . $field_id . '_id">' . $field_meta['title'] . '</label></th>';
+                echo '<th scope="row"><label for="' . esc_attr( $field_id ) . '_id">' . esc_html( $field_meta['title'] ) . '</label></th>';
                 echo '<td>';
-                echo '<select id="' . $field_id . '_id" name="' . $field_id . '">';
-                foreach ($field_meta['possible_values'] as $key => $val) {
-                    $selected = ($field_value == $key) ? 'selected="selected"' : '';
-                    echo "<option value=\"$key\" $selected>$val</option>";
+                echo '<select id="' . esc_attr( $field_id ) . '_id" name="' . esc_attr( $field_id ) . '">';
+                foreach ( $field_meta['possible_values'] as $key => $val ) {
+                    $selected = selected( $field_value, $key, false );
+                    printf( '<option value="%s" %s>%s</option>', esc_attr( $key ), $selected, esc_html( $val ) );
                 }
                 echo '</select>';
                 echo '</td>';
