047d6d44a03a7108f12d15f12ee76f61e9b8951c
Ronald Huereca <ronalfy@gmail.com>
2015-11-01 10:30:24 -0600

    Adding JS regex.

--- js/postmatic-social-login.js
+++ js/postmatic-social-login.js
@@ -19,8 +19,12 @@ function pmsLoadCommentForm($, accessTokenRequestUrl) {
     var comment = $( '#comment' ).val();
     $.get(accessTokenRequestUrl, function (data) {
         container.replaceWith($(data));
-        $( '.comment-form-pms-twitter-extra' ).show().find( 'input' ).attr( 'name', 'email' );
-        $( '.comment-form-email' ).remove();
+        
+        var patt = new RegExp( "twitter" );
+        if ( patt.test( accessTokenRequestUrl ) ) {
+            $( '.comment-form-pms-twitter-extra' ).show().find( 'input' ).attr( 'name', 'email' );
+            $( '.comment-form-email' ).remove();
+        }
         $( '#comment' ).val( comment ).focus();
     });
 }
